7a987c6e5c2ba7542d6039b386b695e108159945
Евраз <kostousik@mail.ru>
2019-08-04 22:13:06 +0300

    Создает шапку и баннер макета

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,11 @@
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
-var rename = require("gulp-rename");
 var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
 var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -21,19 +16,9 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
-});
-
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
 });
 
 gulp.task("server", function () {
@@ -48,26 +33,5 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("build", gulp.series(
-  "clean",
-  "copy",
-  "css",
-  "html"
-));
 
 gulp.task("start", gulp.series("css", "server"));
